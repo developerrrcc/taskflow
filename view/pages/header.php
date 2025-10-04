cba2d9f205bd3ac760657e3f8ff34175199b1eec
Rolin <rolindeveloper@gmail.com>
2025-10-03 22:53:00 -0500

    1

--- view/pages/header.php
+++ view/pages/header.php
@@ -1,4 +1,4 @@
-<section class="w3-content w3-container w3-danger w3-padding">
+<section class="w3-content w3-container w3-blue w3-padding">
     <h1><b>Task</b> <span class="w3-text-black"><b>Flow</b></span> <i class="fa-solid fa-list-check"></i>
     </h1>
     <span class="w3-right w3-tiny">Version 1.0.0</span>
